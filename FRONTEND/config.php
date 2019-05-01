eb544dae100c14322ff63ca963931cb24881331b
CarloCapuz <36867035+CarloCapuz@users.noreply.github.com>
2019-04-30 23:46:59 -0400

    Add files via upload

---
+++ FRONTEND/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'places');
+define('DBUSER', 'testuser');
+define('DBPASS', 'midtermproject');
+define('DBCONNSTRING','mysql:dbname=places;charset=utf8mb4;');
+?>
